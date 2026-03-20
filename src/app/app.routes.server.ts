4421fe4794ee5a6587688b011665fb88c3b31f69
Babli Sharma <bablithv101@gmail.com.com>
2026-03-20 17:36:35 +0530

    some changes in app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'leads',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'add-lead',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'edit-lead/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,29 @@ import { Login } from './auth/login/login';
 import { Dashboard } from './admin/dashboard/dashboard';
 import { Leads } from './admin/leads/leads';
 import { AddLead } from './admin/add-lead/add-lead';
-import { authGuard } from './guards/auth-guard';
 import { EditLead } from './admin/edit-lead/edit-lead';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-    { path: '', component: Login },
-    {
-        path: 'dashboard',
-        component: Dashboard,
-        canActivate: [authGuard]
-    },
-    { path: 'leads', component: Leads },
-    { path: 'add-lead', component: AddLead },
-    { path: 'edit-lead/:id', component: EditLead}
+  {
+    path: '',
+    component: Login
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'leads',
+    component: Leads
+  },
+  {
+    path: 'add-lead',
+    component: AddLead
+  },
+  {
+    path: 'edit-lead/:id',
+    component: EditLead
+  }
 ];
